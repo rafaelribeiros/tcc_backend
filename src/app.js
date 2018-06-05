4f91c38d8259ac611528221b6e545cfde4032000
DESKTOP-D8V1DPN\Rafael <rafael.ribeiro_s@hotmail.com>
2018-06-04 21:01:56 -0300

    inclusão da rota de report.

--- src/app.js
+++ src/app.js
@@ -44,7 +44,7 @@ app.use('/', index);
 app.use('/user', users);
 app.use('/post', posts);
 app.use('/comment', comments);
-
+app.use('/report', reports);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
